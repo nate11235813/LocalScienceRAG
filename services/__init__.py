6fa261f2e6befa9ea7a2eff33d272b9d38a1cd66
Nathaniel Hunt <nathaniel.hunt@gmail.com>
2025-08-07 11:25:50 -0500

    Revert "Initial local RAG project"
    This reverts commit 5fa5331429b91dfc6fcc3d95a9b5120fc587af61.

--- services/__init__.py
+++
@@ -1,9 +0,0 @@
-"""Service layer modules for the RAG application."""
-
-from .document_indexer import DocumentIndexer
-from .rag_service import RAGService
-
-__all__ = [
-    "DocumentIndexer",
-    "RAGService",
-]
--- utils/__init__.py
+++
@@ -1,9 +0,0 @@
-"""Utility modules for the RAG application."""
-
-from .config_loader import load_config
-from .logging_config import setup_logging
-
-__all__ = [
-    "load_config",
-    "setup_logging",
-]
